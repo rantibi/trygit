e64d1ec7131d5dd3839b85337774b692a74c2c5f
rantibi <ran.tibi@loomsystems.com>
2015-06-07 09:58:11 +0300

    4rd log

--- CompletableExam.java
+++ CompletableExam.java
@@ -22,7 +22,7 @@ public class CompletableExam {
             long start = System.currentTimeMillis();
             while (System.currentTimeMillis() - start < 1000)
             System.out.println("bbb");
-            refdsffsdturn "65";
+            return "65";
         }, executor2);
 
 
